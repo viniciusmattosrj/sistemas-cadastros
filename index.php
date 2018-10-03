637392851b8b42a91a31fea6ac33aff92c3a7f79
Vinicius Mattos <vinimattos.rj@gmail.com>
2018-10-03 00:39:50 -0300

    PHP03102018 - Commit inicial projeto sistema de cadastro.

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!-- CSS Materialize -->
+    <link rel="stylesheet" type="text/css" media="screen" href="materialize/css/materialize.min.css" />
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Index</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+
+    <nav class="blue-grey">
+        <div class="nav-wrapper container">
+            <div class="brand-logo light">Sistema de Cadastro</div>
+            <ul class="right">
+                <li><a href=""><i class="material-icons left">account_circle</i>Cadastro</a></li>
+                <li><a href=""><i class="material-icons left">search</i>Consultas</a></li>
+            </ul>
+        </div>
+    </nav>
+
+
+<!--
+    <form action="" method="">
+        <label for=""></label>
+        <input type="text" />
+    </form>
+    -->
+
+    <!-- Arquivos Jquery Javascript -->
+    <script type="text/Javascript" src="materialize/js/jquery-3.3.1.min.js"></script>
+    <script type="text/Javascript" src="materialize/js/materialize.min.js"></script>
+
+    <!-- Inicialização Jquery-->
+    <script type="text/Javascript" src="materialize/js/materialize.min.js">
+        $(document).ready(function(){
+
+        });
+    </script>
+</head>
+</body>
+</html>
